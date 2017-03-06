cd9926b83047561c2d37f70d4d404cf69815748a
Maradona Morais <maradona.morais@hotmail.com>
2017-03-06 10:48:28 -0300

    env config

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import config from './config.js';
 
+const app = express();
+
+mongoose.connect(config.mongodb.uri);
+
+app.use(bodyParser.urlencoded(config.bodyParser));
+app.use(bodyParser.json());
+app.use(compression());
+
+app.listen(config.server.port, ()=>{
+  console.log(`Running at ${config.server.port}`);
+});
+
+export default app;
